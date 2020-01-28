a27778779bb6d72e6d87dd5c120208e6df973374
Raild3x <47638612+Raild3x@users.noreply.github.com>
2020-01-28 11:32:10 -0500

    Added dynamic 'active' setting for navbar

--- CodingFiles/Javascript/HeaderBar.js
+++ CodingFiles/Javascript/HeaderBar.js
@@ -1,11 +1,31 @@
+const str = "id=\"active\"";
+var dict = {};
+dict["Home"] = "";
+dict["FAQ"] = "";
+dict["Bot Control"] = "";
+dict["About Us"] = "";
+dict["Contact Us"] = "";
+dict["Profile"] = "";
+dict["Login"] = "";
+// Set the current page to have the active id
+var currentPage = document.title;
+for (var key in dict) {
+    // check if the property/key is defined in the object itself, not in parent
+    if (dict.hasOwnProperty(key)) {           
+        if(currentPage.search(key) >= 0){
+            dict[key] = str;
+        }
+    }
+}
+
 document.write( '<div class=\"header\">\n' );
 document.write( '		<img class=\"icon\" src=\"https://antisocialnorm.net/ASN_Rat.png\" alt=\"RAT\">\n' );
 document.write( '		<div class=\"navbar\">\n' );
-document.write( '			<a class=\"navbar-item\" href=\"ASN-HomePage.html\">HOME</a>\n' );
-document.write( '			<a class=\"navbar-item\" href=\"\">FAQ</a>\n' );
-document.write( '			<a class=\"navbar-item\" href=\"\">BOT CONTROL</a>\n' );
+document.write( '			<a class=\"navbar-item\"'+dict.Home+' href=\"ASN-HomePage.html\">HOME</a>\n' );
+document.write( '			<a class=\"navbar-item\"'+dict.FAQ+' href=\"\">FAQ</a>\n' );
+document.write( '			<a class=\"navbar-item\"'+dict["Bot Control"]+' href=\"\">BOT CONTROL</a>\n' );
 document.write( '			<div class=\"navbar-dropdown\">\n' );
-document.write( '				<a class=\"navbar-item\">ABOUT US</a>\n' );
+document.write( '				<a class=\"navbar-item\"'+dict["About Us"]+' href=\"\">ABOUT US</a>\n' );
 document.write( '				<div>\n' );
 document.write( '					<div>\n' );
 document.write( '						<a>Server History</a>\n' );
@@ -14,9 +34,9 @@ document.write( '						<a>Michael</a>\n' );
 document.write( '					</div>\n' );
 document.write( '				</div>\n' );
 document.write( '			</div>\n' );
-document.write( '			<a class=\"navbar-item\" href=\"\">CONTACT US</a>\n' );
+document.write( '			<a class=\"navbar-item\"'+dict["Contact Us"]+' href=\"\">CONTACT US</a>\n' );
 document.write( '\n' );
-document.write( '			<a class=\"navbar-item\" id=\"active\" style=\"float:right;right:50px;position:relative;\" href=\"ASN-LoginPage.html\">LOGIN</a>\n' );
-document.write( '			<a class=\"navbar-item\" style=\"float:right;right:50px;position:relative;\" href=\"\">UserProfile</a>\n' );
+document.write( '			<a class=\"navbar-item\"'+dict.Login+' style=\"float:right;right:50px;position:relative;\" href=\"ASN-LoginPage.html\">LOGIN</a>\n' );
+document.write( '			<a class=\"navbar-item\"'+dict.Profile+' style=\"float:right;right:50px;position:relative;\" href=\"\">UserProfile</a>\n' );
 document.write( '		</div>\n' );
 document.write( '	</div>' );
