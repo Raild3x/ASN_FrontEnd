b125706b81900064b95f1267195873ddfd399796
Raild3x <47638612+Raild3x@users.noreply.github.com>
2020-02-11 10:26:59 -0500

    Started VideoMenu page

---
+++ CodingFiles/Javascript/VideoMenu.js
@@ -0,0 +1,24 @@
+
+
+function clamp(num, min, max) {
+	return num <= min ? min : num >= max ? max : num;
+}
+
+function SetupMovies(){
+	
+}
+
+function SetupShows(){
+
+}
+
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+//DisplayStats();
+function loadPage(){
+	console.log("Page Loaded");
+}
